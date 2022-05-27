6292e87f0216407bfed5940af0764e84f7be993c
serepet <mrizkiii64@gmail.com>
2022-05-27 17:41:14 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Perekam Surat Masuk</p>
+</div>
+
+<?=template_footer()?>
